59543a7af5d169ac7f7a5823c038ebfb91ab1e6c
unknown <nelsondiasdemedeiros@gmail.com>
2020-03-07 20:56:41 -0300

    conflict: resolve conflict

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+
 import Header from '../../components/header/Header';
+import Banner from '../../components/banner/Banner';
+import About from '../../components/about/About';
 
 
 class Home extends Component {
     
     render() {
         return (
-            <Header></Header>
+            <>
+                <Header></Header>
+                <Banner></Banner>
+                <About></About>
+            </>
         );
     }
 }
