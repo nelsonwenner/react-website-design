44350f3e5bdedb716c25d482d3fff7a0bc7e6ab1
unknown <nelsondiasdemedeiros@gmail.com>
2020-03-07 20:50:05 -0300

    chore: Create template banner and about

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+
+class Banner extends Component {
+
+    render() {
+        return (
+            <section className="banner" id="home">
+                <h2>We Make <br></br> <span>Brands</span> Matter</h2>
+            </section>
+        );
+    }
+}
+
+export default Banner;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import './style.css';
 
 
 class Header extends Component {
+    
+    componentDidMount() {
+        window.addEventListener('scroll', () => {
+            const isTop = window.scrollY > 0;
+            const header = document.getElementById('header');
+            
+            if (isTop) {
+                header.classList.add('sticky');
+            } else {
+                header.classList.remove('sticky');
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll');
+    }
 
     render() {
         return (
-            <header className="header">
+            <header id="header">
                 <a href="#logo" className="logo">Logo</a>
                 <ul>
                     <li><a href="#home">Home</a></li>
