73744f7fee73e0609592cd1719ec094227467070
unknown <nelsondiasdemedeiros@gmail.com>
2020-03-08 10:38:47 -0300

    create stats

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,7 @@ import Header from '../../components/header/Header';
 import Banner from '../../components/banner/Banner';
 import About from '../../components/about/About';
 import Service from '../../components/service/Service';
+import Stats from '../../components/stats/Stats';
 
 
 class Home extends Component {
@@ -15,6 +16,7 @@ class Home extends Component {
                 <Banner></Banner>
                 <About></About>
                 <Service></Service>
+                <Stats></Stats>
             </>
         );
     }
